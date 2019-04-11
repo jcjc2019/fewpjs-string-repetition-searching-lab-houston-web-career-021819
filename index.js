4c165fb4f6595b679b8529611fb90387bb9a70ae
JingChen <jingchenjc2019@gmail.com>
2019-04-11 17:49:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude(str){
+    // let str = "Na na na na na na na, na na na na, hey Jude.";
+    return str.repeat(16);
+}
+
+function prohibitedLanguage(str){
+    let myRegExp = /candycorn|brusselssprouts/;
+    return str.search(myRegExp);
+}
